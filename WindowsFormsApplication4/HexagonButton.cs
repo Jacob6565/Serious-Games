8f2215388a6e1bdc8fd11935674dca528d78d835
SCARRESS-PC\Scarress <mjesse12@student.aau.dk>
2017-04-18 15:45:55 +0200

    Added check to Map.c which sets isEdge in HexagonButton.c

--- WindowsFormsApplication4/HexagonButton.cs
+++ WindowsFormsApplication4/HexagonButton.cs
@@ -68,7 +68,7 @@ namespace WindowsFormsApplication4
 
         public void NotPassable(object sender, MouseEventArgs e)
         {
-			Console.WriteLine($"You pressed on tile: ({XCoordinate}, {YCoordinate})");
+			Console.WriteLine($"You pressed on tile: ({XCoordinate}, {YCoordinate} {IsEdgeTile})");
             HexagonButton sender_Button = sender as HexagonButton;
             sender_Button.BackColor = Color.FromArgb(255, 105, 180);
             sender_Button.Enabled = false;
