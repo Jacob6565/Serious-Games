17ba264cab6a25e7b79597f9a4a699a1904e4433
TOBIASPALLUDAN\Tobias <tpallu16@student.aau.dk>
2017-05-08 14:18:25 +0200

    Added sesion, round, and interface

---
+++ P2SeriosuGame/IFetchData.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P2SeriosuGame
+{
+	interface IFetchData
+	{
+		Session GetSessionData(int SessionID);
+	}
+}
