ca09f8b7a4e8f1ab101ec9cf42f7e4375babfa09
kristoffer <kristoffer@ACER>
2017-05-08 15:25:35 +0200

    we try

---
+++ P2SeriousGame/IPathFinding.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P2SeriousGame
+{
+    public interface IPathFinding
+    {
+        /*
+         int CheckParent(int count, HexagonButton hexToCheck);
+         List<HexagonButton> ChooseRouteByRand(List<HexagonButton> routes);
+         List<HexagonButton> FindShortestRoutes(List<HexagonButton> edgeHexList);
+         List<HexagonButton> FindLongestRoutes(List<HexagonButton> reachableHexList);
+         HexagonButton FindTheRoute(List<HexagonButton> pathsToEdge, List<HexagonButton> reachableHexList);
+         void CalculateRoutes(HexagonButton[,] hexMap, HexagonButton startingHex);
+		         
+         */
+    }
+}
