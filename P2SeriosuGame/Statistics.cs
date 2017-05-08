f608dd131ef6fc3103c8ff8d353fed97d6ff88d2
JG <jsande16@student.aau.dk>
2017-05-08 15:52:46 +0200

    Statistics

---
+++ P2SeriosuGame/Statistics.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P2SeriousGame
+{
+    public class Statistics
+    {
+        IFetchData fetch;
+        public Statistics(IFetchData fetch)
+        {
+            this.fetch = fetch;
+        }
+
+        public Array GetDataArray(int sessionID)
+        {
+            Session session = fetch.GetSessionData(sessionID);
+            return new int[10];
+        }
+
+    }
+}
