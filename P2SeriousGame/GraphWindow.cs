147a003463f5098e66a4575c70e376c7cc38f8e8
TOBIASPALLUDAN\Tobias <tpallu16@student.aau.dk>
2017-05-10 10:11:31 +0200

    Rollback

---
+++ P2SeriousGame/GraphWindow.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace P2SeriousGame
+{
+	public partial class GraphWindow : Form
+	{
+		public GraphWindow()
+		{
+			InitializeComponent();
+		}
+
+		private void GraphWindow_Load(object sender, EventArgs e)
+		{
+
+		}
+	}
+}
