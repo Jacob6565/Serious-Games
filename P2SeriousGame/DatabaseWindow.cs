b518a1189223535ebba091361a26bae6b39bf9d3
Casper <World@DESKTOP-TTS38P3>
2017-05-16 23:04:45 +0200

    bedtime night night

--- P2SeriousGame/DatabaseWindow.cs
+++ P2SeriousGame/DatabaseWindow.cs
@@ -10,42 +10,10 @@ namespace P2SeriousGame
         public DatabaseWindow()
         {
             InitializeComponent();
-
-
-
-            
-            
-
-            //connectionString="metadata=res://*/SQL.Entities.csdl|res://*/SQL.Entities.ssdl|res://*/SQL.Entities.msl;provider=System.Data.SqlClient;provider connection string=&quot;data source=p2-avengers.database.windows.net;initial catalog=p2-database;persist security info=True;user id=tuandrengen;password=Aouiaom17;MultipleActiveResultSets=True;App=EntityFramework&quot;" providerName="System.Data.EntityClient"
-
-            /*
-            connectionString = ConfigurationManager.ConnectionStrings["Data Source=p2-avengers.database.windows.net;" +
-                "Initial Catalog=p2-database;" +
-                "User ID=tuandrengen;" +
-                "Password=Aouiaom17;"].ConnectionString;*/
-
-            /*
-            connection.ConnectionString =
-                "Data Source=p2-avengers.database.windows.net;" +
-                "Initial Catalog=p2-database;" +
-                "User ID=tuandrengen;" +
-                "Password=Aouiaom17;";*/
-
-
-            /*
-            // Hentet fra app.confiq, som vi lavede da vi tilføjede reference database... - Bruger til at kommunikerer med vores database
-            connectionString = ConfigurationManager.ConnectionStrings["Data Source=p2-avengers.database.windows.net;" +
-                "Initial Catalog=p2-database;" +
-                //"Persist Security Info=True;" +
-                "User ID=tuandrengen;" +
-                "Password=Aouiaom17;"].ConnectionString; */
         }
 
-
-        // SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder();
-        SqlConnection con = new SqlConnection();
         SqlConnection connection = new SqlConnection();
-        string connectionString;
+
 
         SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder()
         {
@@ -90,25 +58,27 @@ namespace P2SeriousGame
             }
         }
 
-        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+
+
+        private void PopulateDataGrid()
         {
             string query = "SELECT * FROM Person";
 
-            using (connection = new SqlConnection(con.ConnectionString))
+            using (connection = new SqlConnection(builder.ConnectionString))
             using (SqlCommand command = new SqlCommand(query, connection))
             using (SqlDataAdapter adapter = new SqlDataAdapter(command))
             {
+
                 DataTable testTable = new DataTable();
                 adapter.Fill(testTable);
                 this.dataGridView1.DataSource = testTable;
-
-
             }
         }
 
         private void DatabaseWindow_Load(object sender, System.EventArgs e)
         {
             PopulatePersons();
+            PopulateDataGrid();
         }
     }
 }
