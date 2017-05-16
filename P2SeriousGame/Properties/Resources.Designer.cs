35a4f7999d9e49cd6335fc685c15877a729de096
SCARRESS-PC\Scarress <mjesse12@student.aau.dk>
2017-05-16 13:42:14 +0200

    plox workz

--- P2SeriousGame/Properties/Resources.Designer.cs
+++ P2SeriousGame/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace P2SeriosuGame.Properties {
+namespace P2SeriousGame.Properties {
     using System;
     
     
