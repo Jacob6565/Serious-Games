bef261d205a3bfe379b760ce462cfe542c42a983
Casper <World@DESKTOP-TTS38P3>
2017-05-16 22:05:40 +0200

    CONNECTIONSTRING ER FUCKING UMULIG

--- P2SeriousGame/DatabaseWindow.cs
+++ P2SeriousGame/DatabaseWindow.cs
@@ -1,12 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Data;
 using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Configuration;
 
 namespace P2SeriousGame
 {
@@ -15,7 +10,39 @@ namespace P2SeriousGame
         public DatabaseWindow()
         {
             InitializeComponent();
+
+
+            
+
+            
+            //con.ConnectionString = ConfigurationManager.ConnectionStrings["metadata = res://*/SQL.Entities.csdl|res://*/SQL.Entities.ssdl|res://*/SQL.Entities.msl;provider=System.Data.SqlClient;provider connection string=&quot;data source=p2-avengers.database.windows.net;initial catalog=p2-database;persist security info=True;user id=tuandrengen;password=Aouiaom17;MultipleActiveResultSets=True;App=EntityFramework&quot; providerName = System.Data.EntityClient"].ToString();
+
+            /*
+            connectionString = ConfigurationManager.ConnectionStrings["Data Source=p2-avengers.database.windows.net;" +
+                "Initial Catalog=p2-database;" +
+                "User ID=tuandrengen;" +
+                "Password=Aouiaom17;"].ConnectionString;*/
+
+            /*
+            connection.ConnectionString =
+                "Data Source=p2-avengers.database.windows.net;" +
+                "Initial Catalog=p2-database;" +
+                "User ID=tuandrengen;" +
+                "Password=Aouiaom17;";*/
+
+
+            /*
+            // Hentet fra app.confiq, som vi lavede da vi tilføjede reference database... - Bruger til at kommunikerer med vores database
+            connectionString = ConfigurationManager.ConnectionStrings["Data Source=p2-avengers.database.windows.net;" +
+                "Initial Catalog=p2-database;" +
+                //"Persist Security Info=True;" +
+                "User ID=tuandrengen;" +
+                "Password=Aouiaom17;"].ConnectionString; */
         }
+
+        SqlConnection con = new SqlConnection();
+        SqlConnection connection = new SqlConnection();
+        string connectionString;
 
         private static DatabaseWindow _instance;
         public static DatabaseWindow GetInstance()
@@ -23,6 +50,22 @@ namespace P2SeriousGame
             if (_instance == null) _instance = new DatabaseWindow();
             {
                 return _instance;
+            }
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            string query = "SELECT * FROM Person";
+
+            using (connection = new SqlConnection(con.ConnectionString))
+            using (SqlCommand command = new SqlCommand(query, connection))
+            using (SqlDataAdapter adapter = new SqlDataAdapter(command))
+            {
+                DataTable testTable = new DataTable();
+                adapter.Fill(testTable);
+                this.dataGridView1.DataSource = testTable;
+
+
             }
         }
     }
